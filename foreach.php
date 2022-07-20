70547f173fc88e8af699332488e5f32c273db7ec
andrealves76 <gynryu76@gmail.com>
2022-07-20 16:29:06 -0300

    Utilizando os comandos 'for' e 'foreach' para construir pequenos algoritmos.

---
+++ foreach.php
@@ -0,0 +1,10 @@
+<?php
+// Utilizando o comando "foreach"( para navegar em um array.
+
+ $frutas = array('laranja','maçã','goiaba','banana' );
+ echo "As frutas contidas no array são: " . PHP_EOL;
+
+ foreach ($frutas as $valor) {
+   echo $valor . PHP_EOL;
+ }
+ ?>
---
+++ lacoFor.php
@@ -0,0 +1,10 @@
+<?php
+// Criar um algoritmo utilizando o laço "for" que mostre os valores de uma tabuada..
+
+// Tabuada do número 7
+
+for ($i=0; $i <=10 ; $i++) {
+  echo " 7 x $i =  " . ($i * 7) . PHP_EOL;
+}
+
+?>
