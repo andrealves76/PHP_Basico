b3fe7ce8cf4287e509c712d09b29ac4c8004a53b
andrealves76 <gynryu76@gmail.com>
2022-08-19 08:37:18 -0300

    comandos substring e explod

--- substring.php
+++ substring.php
@@ -1,10 +1,13 @@
 <?php
 
 $email = 'hera@gmail.com';
+$nome = 'André Pereira Alves';
 
 // $posicaoArroba = 4; buscando posição da @ manualmente
 $posicaoArroba = strpos($email,'@',0);
 
 
-echo substr($email,0, $posicaoArroba) . PHP_EOL;
-echo substr($email,$posicaoArroba +1 ) . PHP_EOL;
+//echo substr($email,0, $posicaoArroba) . PHP_EOL;
+//echo substr($email,$posicaoArroba +1 ) . PHP_EOL;
+
+var_dump(explode(' ',$nome));
---
+++ telefones.php
@@ -0,0 +1,7 @@
+<?php
+
+$telefones = ['(11)9999 9999','(21)8888 8888','(19)7777 7777'];
+
+echo implode(PHP_EOL, $telefones) . PHP_EOL;
+echo implode (' ',$telefones);
+
